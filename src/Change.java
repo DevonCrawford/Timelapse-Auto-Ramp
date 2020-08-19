5a18ef4df1432ad0ebdb7033efd95fe37f028953
Andry <49724349+Andry60351@users.noreply.github.com>
2020-08-19 09:02:40 -0700

    Comments are not needed
    The code is pretty descriptive already, the comments are redundant.

--- src/Change.java
+++ src/Change.java
@@ -17,7 +17,6 @@ public class Change {
 	protected int lastListNum, startListNum;
 	protected int totalImages;
 	
-	// Initializing change data
 	public Change(ArrayList<Image> imgs, int sln, int pln) {
 		images = imgs;
 		startListNum = sln;
@@ -25,27 +24,22 @@ public class Change {
 		totalImages = (lastListNum - startListNum) + 1;
 	}
 	
-	// Gets start image of change sequence
 	public int getStartListNum() {
 		return startListNum;
 	}
 	
-	// Gets last image before change
 	public int getLastListNum() {
 		return lastListNum;
 	}
 	
-	// Gets number of images within this change sequence
 	public int getTotalImages() {
 		return totalImages;
 	}
 	
-	// Gets the increments dispersed among all images in change sequence
 	public double getIncrements() {
 		return increments;
 	}
 	
-	// Sets start image of change sequence
 	public void setStartListNum(int sln) {
 		startListNum = sln;
 	}
@@ -62,13 +56,10 @@ public class Change {
 		for(int i = startListNum; i <= lastListNum; i++) {
 			Image currImg = images.get(i);
 			
-			// Gets the xmp file
 			File file = currImg.getXMP();
 			
-			// Creates new value, incremented
 			double newValue = startValue + (increments * (i-startListNum));
 			
-			// Replaces string data with new value
 			String newData = util.replace(file, key, newValue);
 			
 			if(key.equals("Exposure")) {
@@ -80,7 +71,6 @@ public class Change {
 				currImg.setWhiteBalance(whiteBalance);
 			}
 			
-			// Overwrites xmp file with new string data
 			util.writeFile(currImg, newData);
 		}
 	}
